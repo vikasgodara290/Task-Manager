6c2e100d9fa287590755f13052b39e1d8dd8a366
vikasgodara290 <vikasgodara290@gmail.com>
2025-04-09 23:10:18 +0530

    on check of task animation added

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -1,11 +1,25 @@
 import { CiEdit } from "react-icons/ci";
+import { LuCheck } from "react-icons/lu";
+
+
 
 export default function Card() {
   return (
     <>
       <div className="bg-taskBgColor font-normal h-9 w-11/12 rounded-[8px] text-txtColor flex p-1  mx-auto items-center text-[12px] mb-2">
-        <input type="checkbox" className="mr-1 ml-1" />
-        <span>Add new feature</span>
+        <label className="flex ml-1 items-center group hover:cursor-pointer hover:transform hover:translate-x-1 transition-transform duration-700">
+          <input type="checkbox" className="peer hidden" />
+          <div className="mr-1 w-4 h-4 text-taskBgColor text-[12px] rounded-full 
+            peer-checked:block peer-checked:border-green-300 peer-checked:bg-green-300
+            hidden group-hover:block transition duration-500
+            opacity-0 group-hover:opacity-100 peer-checked:opacity-100 border-gray-400
+            border-2">
+            <span className="">
+              <LuCheck/>
+            </span>
+          </div>
+          Add New Feature
+        </label>
         <div className="ml-auto text-[18px] mr-1">
           <CiEdit />
         </div>
